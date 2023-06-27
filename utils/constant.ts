c32a83be3fd4d9ddaa99e23dccb14ffbb6e2372c
Sean <detectivekim1@gmail.com>
2023-06-27 21:48:23 +0900

    refactor: constant value for polygon

--- utils/constant.ts
+++ utils/constant.ts
@@ -7,27 +7,27 @@ export const VOLATILE_R = 1001000000000000000
 export const CLOBER_FACTORY: { [network: string]: string } = {
   [mainnet.id]: '',
   [arbitrum.id]: '0x24aC0938C010Fb520F1068e96d78E0458855111D',
-  [polygon.id]: '',
+  [polygon.id]: '0x24aC0938C010Fb520F1068e96d78E0458855111D',
   [polygonZkEvm.id]: '',
 }
 
 export const CLOBER_FACTORY_V1: { [network: string]: string } = {
   [mainnet.id]: '',
   [arbitrum.id]: '0x93A43391978BFC0bc708d5f55b0Abe7A9ede1B91',
-  [polygon.id]: '',
+  [polygon.id]: '0x93A43391978BFC0bc708d5f55b0Abe7A9ede1B91',
   [polygonZkEvm.id]: '',
 }
 
 export const CHAIN_ID: { [network: string]: number } = {
   [mainnet.id]: 0,
   [arbitrum.id]: arbitrum.id,
-  [polygon.id]: 0,
+  [polygon.id]: polygon.id,
   [polygonZkEvm.id]: 0,
 }
 
 export const SUPPORT_V1_NO: { [network: string]: number } = {
   [mainnet.id]: 0,
   [arbitrum.id]: 13,
-  [polygon.id]: 0,
+  [polygon.id]: 8,
   [polygonZkEvm.id]: 0,
 }
