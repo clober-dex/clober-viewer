0ea263f4ec2e726ade993cbba9716287adfa4c9f
Sean <detectivekim1@gmail.com>
2023-06-27 19:28:51 +0900

    feat: constant for `CloberViewer`

---
+++ utils/constant.ts
@@ -0,0 +1,26 @@
+import { arbitrum, mainnet, polygon, polygonZkEvm } from '@wagmi/chains'
+
+export const VOLATILE_A = 10000000000
+
+export const VOLATILE_R = 1001000000000000000
+
+export const CLOBER_FACTORY: { [network: string]: string } = {
+  [mainnet.id]: '',
+  [arbitrum.id]: '',
+  [polygon.id]: '',
+  [polygonZkEvm.id]: '',
+}
+
+export const CLOBER_FACTORY_V1: { [network: string]: string } = {
+  [mainnet.id]: '',
+  [arbitrum.id]: '',
+  [polygon.id]: '',
+  [polygonZkEvm.id]: '',
+}
+
+export const SUPPORT_V1_NO: { [network: string]: string } = {
+  [mainnet.id]: '',
+  [arbitrum.id]: '',
+  [polygon.id]: '',
+  [polygonZkEvm.id]: '',
+}
